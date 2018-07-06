6a0b656272ffcc864ff9635244d558252d0b9d50
Anur <anurnomeru@163.com>
2018-07-06 17:26:31 +0800

    -

--- examples/src/main/java/kafka/examples/anur/selector/NioExample.java
+++ examples/src/main/java/kafka/examples/anur/selector/NioExample.java
@@ -1,8 +1,14 @@
 package kafka.examples.anur.selector;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.RandomAccessFile;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
@@ -15,18 +21,21 @@ import java.util.Set;
 public class NioExample {
 
     public void selector() throws IOException {
-        ByteBuffer buffer = ByteBuffer.allocate(1024);
-        Selector selector = Selector.open();
-        ServerSocketChannel ssc = ServerSocketChannel.open();
-        ssc.configureBlocking(false);//设置为非阻塞方式
+        ByteBuffer buffer = ByteBuffer.allocate(1024);// 申请1k内存
+
+        Selector selector = Selector.open();// 打开轮询器
+
+        ServerSocketChannel ssc = ServerSocketChannel.open();//
+        ssc.configureBlocking(false);// 设置为非阻塞方式
         ssc.socket()
            .bind(new InetSocketAddress(8080));
-        ssc.register(selector, SelectionKey.OP_ACCEPT);//注册监听的事件
+        ssc.register(selector, SelectionKey.OP_ACCEPT);// 注册监听的事件（注册key到selector）
+
         while (true) {
-            Set selectedKeys = selector.selectedKeys();//取得所有key集合
-            Iterator it = selectedKeys.iterator();
+            Set<SelectionKey> selectedKeys = selector.selectedKeys();// 取得所有注册的key集合
+            Iterator<SelectionKey> it = selectedKeys.iterator();
             while (it.hasNext()) {
-                SelectionKey key = (SelectionKey) it.next();
+                SelectionKey key = it.next();
                 if ((key.readyOps() & SelectionKey.OP_ACCEPT) == SelectionKey.OP_ACCEPT) {
                     ServerSocketChannel ssChannel = (ServerSocketChannel) key.channel();
                     SocketChannel sc = ssChannel.accept();//接受到服务端的请求
@@ -47,6 +56,46 @@ public class NioExample {
                     it.remove();
                 }
             }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        //        File file = new File("xxx");
+        //
+        //        FileInputStream inputStream = new FileInputStream(file);
+
+        RandomAccessFile aFile = new RandomAccessFile("C:/Users/Anur/Desktop/New Text Document.txt", "rw");
+
+        FileChannel inChannel = aFile.getChannel();
+
+        ByteBuffer buf = ByteBuffer.allocate(200);
+        int bytesRead = inChannel.read(buf);
+        while (bytesRead != -1) {
+            System.out.println("Read " + bytesRead);
+            buf.flip();
+            while (buf.hasRemaining()) {
+                System.out.print((char) buf.get());
+            }
+            buf.clear();
+            bytesRead = inChannel.read(buf);
+        }
+        aFile.close();
+
+        File file = new File("C:/Users/Anur/Desktop/New Text Document.txt");
+        InputStream in = null;
+
+        try {
+            System.out.println("以字节为单位读取文件内容，一次读一个字节：");
+            // 一次读一个字节
+            in = new FileInputStream(file);
+            int tempbyte;
+            while ((tempbyte = in.read()) != -1) {
+                System.out.write(tempbyte);
+            }
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
         }
     }
 }
---
+++ examples/src/main/java/kafka/examples/anur/selector/Tester.java
@@ -0,0 +1,142 @@
+package kafka.examples.anur.selector;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+/**
+ * Created by Anur IjuoKaruKas on 7/6/2018
+ */
+public class Tester {
+
+    //    public static void main(String[] args) throws Exception {
+    //        ByteArrayInputStream inputStream = new ByteArrayInputStream("test test test!".getBytes());
+    //        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+    //
+    //        FileOutputStream fileOutputStream = new FileOutputStream("C:\\Users\\Anur\\Desktop\\test.test");
+    //
+    //        int count = 0;
+    //
+    //        int result;
+    //        while ((result = inputStream.read()) != -1) {
+    //            fileOutputStream.write(result);
+    //            System.out.println(count++);
+    //        }
+    //
+    //        System.out.println(outputStream.toString());
+    //
+    //        fileOutputStream.close();
+    //        outputStream.close();
+    //        inputStream.close();
+    //    }
+
+    //    public static void main(String[] args) throws Exception {
+    //        FileInputStream fileInputStream = new FileInputStream("C:\\Users\\Anur\\Desktop\\test.test");
+    //
+    //        FileOutputStream fileOutputStream = new FileOutputStream("C:\\Users\\Anur\\Desktop\\test.test1");
+    //
+    //        byte[] data = new byte[1024 * 2014];
+    //
+    //        int counter = 0;
+    //
+    //        while (fileInputStream.available() > 0) {
+    //            fileInputStream.read(data);
+    //            fileOutputStream.write(data);
+    //            System.out.println(counter++);
+    //        }
+    //
+    //        fileInputStream.close();
+    //        fileOutputStream.close();
+    //    }
+
+    public static void main(String[] args) throws Exception {
+
+        long sta = System.currentTimeMillis();
+
+        //        RandomAccessFile aFile = new RandomAccessFile("C:\\Users\\Anur\\Desktop\\centos.iso", "rw");
+        RandomAccessFile bFile = new RandomAccessFile("C:\\Users\\Anur\\Desktop\\test.txt", "rw");
+
+        //        FileChannel fileChannela = aFile.getChannel();
+        FileChannel fileChannelb = bFile.getChannel();
+        long size = fileChannelb.size();
+
+        ByteBuffer byteBuffer = ByteBuffer.allocate(200);
+        byteBuffer.put("这是一个测试".getBytes("utf-8"));
+
+        int count = 0;
+        byteBuffer.flip();
+//        fileChannelb.write(byteBuffer, size);
+//        byteBuffer.rewind();
+        while (byteBuffer.hasRemaining()) {
+            System.out.println(count++);
+            fileChannelb.write(byteBuffer, size);
+        }
+        //        fileChannelb.transferFrom(fileChannela, 0, size);
+        //
+        long e = System.currentTimeMillis();
+
+        System.out.println(e - sta);
+
+        //        long sta = System.currentTimeMillis();
+        //        FileInputStream fileInputStream = new FileInputStream("C:\\Users\\Anur\\Desktop\\centos.iso");
+        //
+        //        FileOutputStream fileOutputStream = new FileOutputStream("C:\\Users\\Anur\\Desktop\\centos.bak.iso");
+        //        int result = fileInputStream.read();
+        //
+        //        while (result != -1) {
+        //            fileOutputStream.write(result);
+        //            result = fileInputStream.read();
+        //        }
+        //        long e = System.currentTimeMillis();
+        //
+        //        System.out.println(sta - e);
+        //
+        //        long start = System.currentTimeMillis();
+        //        BufferedInputStream inputStream = new BufferedInputStream(new FileInputStream("C:\\Users\\Anur\\Desktop\\test.rar"));
+        //
+        //        BufferedOutputStream outputStream = new BufferedOutputStream(new FileOutputStream("C:\\Users\\Anur\\Desktop\\test1.rar"));
+        //
+        //        byte[] result = new byte[1];
+        //
+        //        int counter = 0;
+        //
+        //        while (inputStream.available() > 0) {
+        //            inputStream.read(result);
+        //            outputStream.write(result);
+        //        }
+        //
+        //        inputStream.close();
+        //        outputStream.close();
+        //
+        //        long end = System.currentTimeMillis();
+        //
+        //        System.out.println(end - start);
+        //
+        //        start = System.currentTimeMillis();
+        //
+        //        FileInputStream fileInputStream = new FileInputStream("C:\\Users\\Anur\\Desktop\\test.rar");
+        //        FileOutputStream fileOutputStream = new FileOutputStream("C:\\Users\\Anur\\Desktop\\test2.rar");
+        //
+        //        byte[] result1 = new byte[1];
+        //
+        //        int counter1 = 0;
+        //
+        //        while (fileInputStream.available() > 0) {
+        //            fileInputStream.read(result1);
+        //            fileOutputStream.write(result1);
+        //        }
+        //
+        //        fileInputStream.close();
+        //        fileOutputStream.close();
+        //
+        //        end = System.currentTimeMillis();
+        //
+        //        System.out.println(end - start);
+    }
+}
