05810d042ea33811e0d4c404a26244b2d1ed2950
Anur <anurnomeru@163.com>
2018-08-02 18:08:34 +0800

    -

--- examples/src/main/java/kafka/examples/Producer.java
+++ examples/src/main/java/kafka/examples/Producer.java
@@ -6,7 +6,7 @@
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -20,30 +20,21 @@ import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.clients.producer.RecordMetadata;
-import org.apache.kafka.common.serialization.IntegerSerializer;
-import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Properties;
 import java.util.concurrent.ExecutionException;
 
 public class Producer extends Thread {
-
     private final KafkaProducer<Integer, String> producer;
-
     private final String topic;
-
     private final Boolean isAsync;
-
-    public static void main(String[] args) {
-        System.out.println(IntegerSerializer.class.getName());
-    }
 
     public Producer(String topic, Boolean isAsync) {
         Properties props = new Properties();
         props.put("bootstrap.servers", "localhost:9092");
         props.put("client.id", "DemoProducer");
-        props.put("key.serializer", IntegerSerializer.class.getName());
-        props.put("value.serializer", StringSerializer.class.getName());
+        props.put("key.serializer", "org.apache.kafka.common.serialization.IntegerSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         producer = new KafkaProducer<>(props);
         this.topic = topic;
         this.isAsync = isAsync;
@@ -62,8 +53,7 @@ public class Producer extends Thread {
                 try {
                     producer.send(new ProducerRecord<>(topic,
                         messageNo,
-                        messageStr))
-                            .get();
+                        messageStr)).get();
                     System.out.println("Sent message: (" + messageNo + ", " + messageStr + ")");
                 } catch (InterruptedException | ExecutionException e) {
                     e.printStackTrace();
@@ -77,9 +67,7 @@ public class Producer extends Thread {
 class DemoCallBack implements Callback {
 
     private final long startTime;
-
     private final int key;
-
     private final String message;
 
     public DemoCallBack(long startTime, int key, String message) {
@@ -93,8 +81,8 @@ class DemoCallBack implements Callback {
      * be called when the record sent to the server has been acknowledged. Exactly one of the arguments will be
      * non-null.
      *
-     * @param metadata The metadata for the record that was sent (i.e. the partition and offset). Null if an error
-     * occurred.
+     * @param metadata  The metadata for the record that was sent (i.e. the partition and offset). Null if an error
+     *                  occurred.
      * @param exception The exception thrown during processing of this record. Null if no error occurred.
      */
     public void onCompletion(RecordMetadata metadata, Exception exception) {
