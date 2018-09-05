894d3e1247595a7a6e4c3548209911ac265c3267
Anur <anurnomeru@163.com>
2018-09-05 15:16:52 +0800

    -

--- clients/src/main/java/org/apache/kafka/clients/consumer/internals/ConsumerNetworkClient.java
+++ clients/src/main/java/org/apache/kafka/clients/consumer/internals/ConsumerNetworkClient.java
@@ -159,6 +159,15 @@ public class ConsumerNetworkClient implements Closeable {
     }
 
     /**
+     * Wakeup an active poll. This will cause the polling thread to throw an exception either
+     * on the current poll if one is active, or the next poll.
+     */
+    public void wakeup() {
+        this.wakeup.set(true);
+        this.client.wakeup();
+    }
+
+    /**
      * Ensure our metadata is fresh (if an update is expected, this will block
      * until it has completed).
      */
@@ -166,15 +175,6 @@ public class ConsumerNetworkClient implements Closeable {
         if (this.metadata.updateRequested() || this.metadata.timeToNextUpdate(time.milliseconds()) == 0) {
             awaitMetadataUpdate();
         }
-    }
-
-    /**
-     * Wakeup an active poll. This will cause the polling thread to throw an exception either
-     * on the current poll if one is active, or the next poll.
-     */
-    public void wakeup() {
-        this.wakeup.set(true);
-        this.client.wakeup();
     }
 
     /**
