e99d5fed4781f0e81e4caad68cd17745654b859b
Anur <anurnomeru@163.com>
2018-11-05 19:59:35 +0800

    -

--- core/src/main/scala/kafka/cluster/Replica.scala
+++ core/src/main/scala/kafka/cluster/Replica.scala
@@ -1,42 +1,44 @@
 /**
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
-*/
+  * Licensed to the Apache Software Foundation (ASF) under one or more
+  * contributor license agreements.  See the NOTICE file distributed with
+  * this work for additional information regarding copyright ownership.
+  * The ASF licenses this file to You under the Apache License, Version 2.0
+  * (the "License"); you may not use this file except in compliance with
+  * the License.  You may obtain a copy of the License at
+  *
+  * http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  */
 
 package kafka.cluster
 
-import kafka.log.Log
-import kafka.utils.{SystemTime, Time, Logging}
-import kafka.server.{LogReadResult, LogOffsetMetadata}
-import kafka.common.KafkaException
-
 import java.util.concurrent.atomic.AtomicLong
 
+import kafka.common.KafkaException
+import kafka.log.Log
+import kafka.server.{LogOffsetMetadata, LogReadResult}
+import kafka.utils.{Logging, SystemTime, Time}
+
 class Replica(val brokerId: Int,
-              val partition: Partition,
+              val partition: Partition,// 这个副本的分区
               time: Time = SystemTime,
               initialHighWatermarkValue: Long = 0L,
               val log: Option[Log] = None) extends Logging {
   // the high watermark offset value, in non-leader replicas only its message offsets are kept
+  // HW
   @volatile private[this] var highWatermarkMetadata: LogOffsetMetadata = new LogOffsetMetadata(initialHighWatermarkValue)
   // the log end offset value, kept in all replicas;
   // for local replica it is the log's end offset, for remote replicas its value is only updated by follower fetch
+  // 对于本地副本来说这就是记录追加到Log中最新消息的offset，对远程副本来说需要发送请求来获取这个值
   @volatile private[this] var logEndOffsetMetadata: LogOffsetMetadata = LogOffsetMetadata.UnknownOffsetMetadata
 
-  val topic = partition.topic
-  val partitionId = partition.partitionId
+  val topic: String = partition.topic
+  val partitionId: Int = partition.partitionId
 
   def isLocal: Boolean = {
     log match {
@@ -45,26 +47,33 @@ class Replica(val brokerId: Int,
     }
   }
 
-  private[this] val lastCaughtUpTimeMsUnderlying = new AtomicLong(time.milliseconds)
+  private[this] val lastCaughtUpTimeMsUnderlying = new AtomicLong(time.milliseconds)// 最后一次追上leader的时间戳
 
   def lastCaughtUpTimeMs = lastCaughtUpTimeMsUnderlying.get()
 
-  def updateLogReadResult(logReadResult : LogReadResult) {
-    logEndOffset = logReadResult.info.fetchOffsetMetadata
+  def updateLogReadResult(logReadResult: LogReadResult) {
+    logEndOffset = logReadResult.info.fetchOffsetMetadata// 更新一波LEO
 
     /* If the request read up to the log end offset snapshot when the read was initiated,
      * set the lastCaughtUpTimeMsUnderlying to the current time.
      * This means that the replica is fully caught up.
+     *
+     * 如果当请求读取初始化时，赶上了LEO快照
+     * 将它的lastCaughtUpTimeMsUnderlying设置为当前时间
+     * 这意味着副本赶上了进度
      */
-    if(logReadResult.isReadFromLogEnd) {
+    if (logReadResult.isReadFromLogEnd) {
       lastCaughtUpTimeMsUnderlying.set(time.milliseconds)
     }
   }
 
   private def logEndOffset_=(newLogEndOffset: LogOffsetMetadata) {
     if (isLocal) {
+      // 不能直接更新本地副本的LEO TODO 由 log.get.logEndOffsetMetadata 获取
       throw new KafkaException("Should not set log end offset on partition [%s,%d]'s local replica %d".format(topic, partitionId, brokerId))
     } else {
+
+      // 通过请求进行更新
       logEndOffsetMetadata = newLogEndOffset
       trace("Setting log end offset for replica %d for partition [%s,%d] to [%s]"
         .format(brokerId, topic, partitionId, logEndOffsetMetadata))
@@ -78,7 +87,7 @@ class Replica(val brokerId: Int,
       logEndOffsetMetadata
 
   def highWatermark_=(newHighWatermark: LogOffsetMetadata) {
-    if (isLocal) {
+    if (isLocal) {// 只有本地副本可以更新HW
       highWatermarkMetadata = newHighWatermark
       trace("Setting high watermark for replica %d partition [%s,%d] on broker %d to [%s]"
         .format(brokerId, topic, partitionId, brokerId, newHighWatermark))
@@ -98,16 +107,16 @@ class Replica(val brokerId: Int,
   }
 
   override def equals(that: Any): Boolean = {
-    if(!(that.isInstanceOf[Replica]))
+    if (!(that.isInstanceOf[Replica]))
       return false
     val other = that.asInstanceOf[Replica]
-    if(topic.equals(other.topic) && brokerId == other.brokerId && partition.equals(other.partition))
+    if (topic.equals(other.topic) && brokerId == other.brokerId && partition.equals(other.partition))
       return true
     false
   }
 
   override def hashCode(): Int = {
-    31 + topic.hashCode() + 17*brokerId + partition.hashCode()
+    31 + topic.hashCode() + 17 * brokerId + partition.hashCode()
   }
 
 
@@ -117,7 +126,7 @@ class Replica(val brokerId: Int,
     replicaString.append("; Topic: " + topic)
     replicaString.append("; Partition: " + partition.partitionId)
     replicaString.append("; isLocal: " + isLocal)
-    if(isLocal) replicaString.append("; Highwatermark: " + highWatermark)
+    if (isLocal) replicaString.append("; Highwatermark: " + highWatermark)
     replicaString.toString()
   }
 }
--- core/src/main/scala/kafka/server/LogOffsetMetadata.scala
+++ core/src/main/scala/kafka/server/LogOffsetMetadata.scala
@@ -1,19 +1,19 @@
 /**
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+  * Licensed to the Apache Software Foundation (ASF) under one or more
+  * contributor license agreements.  See the NOTICE file distributed with
+  * this work for additional information regarding copyright ownership.
+  * The ASF licenses this file to You under the Apache License, Version 2.0
+  * (the "License"); you may not use this file except in compliance with
+  * the License.  You may obtain a copy of the License at
+  *
+  * http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  */
 
 package kafka.server
 
@@ -25,7 +25,7 @@ object LogOffsetMetadata {
   val UnknownFilePosition = -1
 
   class OffsetOrdering extends Ordering[LogOffsetMetadata] {
-    override def compare(x: LogOffsetMetadata , y: LogOffsetMetadata ): Int = {
+    override def compare(x: LogOffsetMetadata, y: LogOffsetMetadata): Int = {
       x.offsetDiff(y).toInt
     }
   }
@@ -37,7 +37,7 @@ object LogOffsetMetadata {
  *  1. the message offset
  *  2. the base message offset of the located segment
  *  3. the physical position on the located segment
- */
+*/
 case class LogOffsetMetadata(messageOffset: Long,
                              segmentBaseOffset: Long = LogOffsetMetadata.UnknownSegBaseOffset,
                              relativePositionInSegment: Int = LogOffsetMetadata.UnknownFilePosition) {
@@ -66,9 +66,9 @@ case class LogOffsetMetadata(messageOffset: Long,
   // compute the number of bytes between this offset to the given offset
   // if they are on the same segment and this offset precedes the given offset
   def positionDiff(that: LogOffsetMetadata): Int = {
-    if(!onSameSegment(that))
+    if (!onSameSegment(that))
       throw new KafkaException(s"$this cannot compare its segment position with $that since they are not on the same segment")
-    if(messageOffsetOnly())
+    if (messageOffsetOnly())
       throw new KafkaException(s"$this cannot compare its segment position with $that since it only has message offset info")
 
     this.relativePositionInSegment - that.relativePositionInSegment
